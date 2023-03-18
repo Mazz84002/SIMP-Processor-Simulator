23bc54b397111b169b3bb62bd4bcb3c0454eff32
Mazz84002 <93880850+Mazz84002@users.noreply.github.com>
2023-03-19 00:04:06 +0200

    makefile fixed

---
+++ build/CMakeFiles/simp-processor-simulator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simp-processor-simulator.dir/sim.c.o"
+  "CMakeFiles/simp-processor-simulator.dir/sim.c.o.d"
+  "simp-processor-simulator"
+  "simp-processor-simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simp-processor-simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/simp-processor-simulator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for simp-processor-simulator.
+# This may be replaced when dependencies are built.
